e6f91fa93c3771b29f0f6a2fc828e63658025964
Alejandra Cabrera <alejandra.cabrera@zapier.com>
2020-12-23 07:59:39 -0500

    adding summary

--- Scripts/data_explore.R
+++ Scripts/data_explore.R
@@ -12,6 +12,7 @@ names(practice_r)
 str(practice_r)
 head(practice_r$year)
 tail(practice_r$year)
+summary(practice_r$year)
 
 ##modify columns types
 practice_r$year_numeric <- as.numeric(practice_r$year)
